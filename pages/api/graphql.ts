84a5b09c1229208682174c423e3f34360c33eae9
sanish <sanishkr@gmail.com>
2022-10-23 00:05:41 +0530

    :ambulance: add instrospection flag

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,19 +1,19 @@
-import  {  ApolloServer  }  from  "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import Cors from 'micro-cors';
 import "graphql-import-node";
 
-import  typeDefs  from  "../../graphql/schema.graphql";
-import  resolvers  from  "../../graphql/resolvers";
+import typeDefs from "../../graphql/schema.graphql";
+import resolvers from "../../graphql/resolvers";
 
 const cors = Cors();
 
-const  apolloServer  =  new  ApolloServer({  typeDefs,  resolvers, plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]  });
+const apolloServer = new ApolloServer({ typeDefs, resolvers, plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], introspection: true });
 
-export  const  config  =  {
-    api:  {
-        bodyParser:  false
-    }
+export const config = {
+  api: {
+    bodyParser: false
+  }
 };
 
 const startServer = apolloServer.start()
